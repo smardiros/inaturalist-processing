25330aba1ae1aa53df659e722680f54febdaa91a
Sam Mardirosian <sam@superb-ai.com>
2022-09-06 17:09:34 -0400

    Create json files per images in SuperbAI format

--- download_images.py
+++ download_images.py
@@ -2,12 +2,14 @@ import csv
 import json
 import os
 from sre_parse import CATEGORIES
+from itertools import islice
 import wget
 
 DOWNLOAD_FOLDER = "images/"
-CSV_FILE = "observations-257982-short.csv"
+CSV_FILE = "observations-257982.csv"
 WORKING_DIRECTORY = os.getcwd()
 PROJECT_JSON = "project.json"
+NUM_IMAGES = 1000
 
 
 with open(PROJECT_JSON) as file:
@@ -25,7 +27,7 @@ with open(CSV_FILE) as file:
     csv_reader = csv.reader(file)
     header = next(csv_reader)
 
-    for row in csv_reader:
+    for row in islice(csv_reader, NUM_IMAGES):
         id = row[0]
         url = row[1]
         species = row[2]
@@ -34,29 +36,31 @@ with open(CSV_FILE) as file:
         if not os.path.exists(image_filepath):
             response = wget.download(url, image_filepath)
 
-        json_filename = id + ".json"
+        json_filename = image_filename + ".json"
         json_filepath = os.path.join(WORKING_DIRECTORY, DOWNLOAD_FOLDER, json_filename)
 
         if species not in category_options:
             continue
 
         img_json = {
-            "objects": [],
-            "categories": {
-                "properties":[
-                    {
-                        "type": category["type"],
-                        "property_id": category["id"],
-                        "property_name": category["name"],
-                        "option_id": category_options[species],
-                        "option_name": species
-                    }
-                ]
+            "result": {
+                "objects": [],
+                "categories": {
+                    "properties":[
+                        {
+                            "type": category["type"],
+                            "property_id": category["id"],
+                            "property_name": category["name"],
+                            "option_id": category_options[species],
+                            "option_name": species
+                        }
+                    ]
+                }
             }
         }
 
         with open(json_filepath, "w") as file:
-            json.dump(img_json, file)
+            json.dump(img_json, file, indent=4)
 
 
 
